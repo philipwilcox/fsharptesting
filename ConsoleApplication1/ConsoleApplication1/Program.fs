9e2607c68f2eedca1dabf714f580d703d9fe0901
philipwilcox <philip@philipwilcox.com>
2016-12-14 11:32:53 -0800

    Let's do some basic math

---
+++ ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+module HelloSquare
+
+let square x = x * x
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%d squared is: %d!" 12 (square 12)
+    0 // return an integer exit code
